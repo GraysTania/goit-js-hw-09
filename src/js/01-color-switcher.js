89b7c18637bce2eed0e530190c27c7a2c51b60f9
GraysTania <101881748+GraysTania@users.noreply.github.com>
2022-12-10 22:14:52 +0200

    2

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -5,7 +5,8 @@ const refs = {
 };
 
 console.log(refs.stop);
-timerId = null;
+
+let timerId = null;
 
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
